d60e691d608069c270a6e9cd4d65e5a850798dd3
Kzu <alvaro.vega.pavez@gmail.com>
2025-02-03 18:48:45 -0300

    update: se agrega metodo senVitalsSing

--- src/main/java/com/alertasmedicas/app/queue_producer/controller/ProducerController.java
+++ src/main/java/com/alertasmedicas/app/queue_producer/controller/ProducerController.java
@@ -1,5 +1,6 @@
 package com.alertasmedicas.app.queue_producer.controller;
 
+import com.alertasmedicas.app.queue_producer.dto.FakerDTO;
 import com.alertasmedicas.app.queue_producer.dto.MeasurementDTO;
 import com.alertasmedicas.app.queue_producer.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Log4j2
 @RestController
@@ -23,16 +26,28 @@ public class ProducerController {
     }
 
     @PostMapping("/send/anomaly")
-    public ResponseEntity<String> send(@RequestBody MeasurementDTO measurementDTO) {
-        log.info("measurementDTO: {}", measurementDTO);
+    public ResponseEntity<String> sendAnomaly(@RequestBody MeasurementDTO measurementDTO) {
+        log.info("Enviando anomalias measurementDTO: {}", measurementDTO);
         try {
-            producerService.sendMessage(measurementDTO);
+            producerService.sendMessageAnomaly(measurementDTO);
             return ResponseEntity.ok("Mensaje enviado");
         } catch (Exception e) {
-            log.info("Error al enviar el paciente: {}", e.getMessage());
+            log.info("Error al enviar anomalias: {}", e.getMessage());
             return ResponseEntity.internalServerError().build();
         }
 
+    }
+
+    @PostMapping("/send/pacientMeasures")
+    public ResponseEntity<String> sendVitalsSigns(@RequestBody List<FakerDTO> fakerList) {
+        log.info("Enviando signos vitales fakerDTO: {}", fakerList);
+        try {
+            producerService.sendMessageVitalsSigns(fakerList);
+            return ResponseEntity.ok("Mensaje enviado");
+        } catch (Exception e) {
+            log.info("Error al enviar signos vitales: {}", e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
